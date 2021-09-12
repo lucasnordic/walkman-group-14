663356643ca1692ae97e16df2a0ed0a28c9d8703
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-09-12 15:28:34 +0200

    some attributes are added to petOwner & petLover.

--- server/Models/PetLover.js
+++ server/Models/PetLover.js
@@ -3,6 +3,8 @@ var Schema = mongoose.Schema;
 
 var petLoverSchema = new Schema({
     username: { type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    fullName: {type: String},
     availableHours: [{ type: Number, required: true}],
     acceptablePets: [{ type: String}]
 });
