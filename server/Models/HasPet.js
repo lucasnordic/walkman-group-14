79f9638722e27bb79fec23d928e9c1e35169e903
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-09-13 14:04:01 +0200

    file modification

--- server/Models/HasPet.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var hasPetSchema = new Schema({
-    ownerId: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true},
-    animalId: [{type: mongoose.Schema.Types.ObjectId, require: true}]
-});
-
-module.exports = mongoose.model('hasPets', hasPetSchema);
