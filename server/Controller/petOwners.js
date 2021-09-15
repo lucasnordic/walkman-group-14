08bf748329fc70b64dec65cddc1cb51a41d9e4d6
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-09-15 22:09:34 +0200

    patch added to petowner

--- server/Controller/petOwners.js
+++ server/Controller/petOwners.js
@@ -8,4 +8,60 @@ exports.postPetOwner = (req, res, next) => {
         if (err) { return next(err); }
         res.status(201).json(petOwner);
     })
-}
+};
+
+
+exports.viewAll = (req, res, next) => {
+    PetOwner.find()
+    .then((result) => {
+        res.json(result);
+        res.send(result);
+    })
+    .catch ((err) => {
+        return next(err);
+    });
+};
+
+exports.viewUser = (req, res, next) => {
+    PetOwner.findById(req.params.userId)
+    .then((result) => {
+        res.json(result);
+        res.send(result);
+    })
+    .catch ((err) => {
+        return next(err);
+    });
+};
+
+exports.deleteAll = (req, res, next) => {
+    PetOwner.deleteMany({})
+    .then((result) => {
+        res.json(result);
+        res.send(result);
+    })
+    .catch ((err) => {
+        return next(err);
+    });
+};
+
+exports.savePet = (req, res, next) => {
+    PetOwner.findOneAndUpdate(req.params.userId, {$push : {Pets: req.body}})
+    .then((result) => {
+        res.json(result);
+        res.send(result);
+    })
+    .catch ((err) => {
+        return next(err);
+    });
+};
+
+exports.editPetOwner = (req, res, next) => {
+    PetOwner.findByIdAndUpdate(req.params.userid, req.body, {new:true})
+    .then((result) => {
+        res.json(result);
+        res.send(result);
+    }).catch ((err) => {
+        return next(err);
+    });
+
+};
--- server/Router/petOwnerRouter.js
+++ server/Router/petOwnerRouter.js
@@ -3,7 +3,29 @@ const router = express.Router();
 const controller = require ('../Controller/petOwners');
 
 router.post('/', controller.postPetOwner);
-router.get('/', (req, res) => {
-    res.send('We want to get rid of our pets just for a short while... Pleeease!... HELP US!');
-});
+
+router.get('/', controller.viewAll);
+
+router.delete('/', controller.deleteAll);
+
+router.get('/:userId', controller.viewUser);
+
+router.patch('/:userId', controller.editPetOwner);
+
+router.post('/:userId/pets', controller.savePet);
+
+
+
+
+/*
+
+(a) POST /camels
+(b) GET /camels
+(c) DELETE /camels
+(d) GET /camels/:id
+(e) PUT /camels/:id
+(f) PATCH /camels/:id
+(g) DELETE /camels/:id
+
+*/
 module.exports = router;
