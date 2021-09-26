357826796a29e164387354bf6e1cd42ae3f36580
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-09-26 16:46:13 +0200

    Pet owner profile page created. Finally!!

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import PetOwnerProfile from './views/PetOwnerProfile.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/petownerprofile',
+      name: 'pet owner profile',
+      component: PetOwnerProfile
+
     }
   ]
 })
---
+++ client/src/views/PetOwnerProfile.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Why don't you work ??</h1>
+</template>
