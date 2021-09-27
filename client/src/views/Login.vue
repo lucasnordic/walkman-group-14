c3b0a0d107677bec14dda22b192817eecac106da
Lucas Nordgren <gusnordglu@student.gu.se>
2021-09-27 17:34:13 +0200

    Register and login pages edited. added a combined style class for both pages. Added a background on both pages.

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,99 +1,80 @@
 <template>
-  <div id="main-div">
-    <b-jumbotron id="register_background">
-      <template #header>Log-in</template>
+  <div>
+    <div id="bg_page">
+      <div id="main_div">
+        <b-jumbotron id="main_jumbo">
+          <template #header>
+            <div id="main_title">
+              <b-form> Log-in</b-form>
+              <hr class="my-4" />
+            </div>
+          </template>
+          <div id="display_json">
+            <pre>{{ petLover }}</pre>
+          </div>
 
-      <!-- <template #lead>
-        <ul>
-          <li><strong>Pet owner</strong>: You are looking for a caretaker</li>
-          <li><strong>Pet lover</strong>: You want to earn some cash</li>
-        </ul>
-      </template> -->
+          <b-form id="main_form" @submit="onSubmit">
+            <b-form-group id="username" label="Username" label-for="i-1">
+              <b-form-input
+                id="i-1"
+                v-model="form.username"
+                required
+              ></b-form-input>
+            </b-form-group>
 
-      <hr class="my-4" />
+            <b-form-group id="password" label="Password" label-for="i-2">
+              <b-form-input
+                id="i-2"
+                v-model="form.password"
+                required
+              ></b-form-input>
+            </b-form-group>
 
-      <b-form id="login_form" @submit="onSubmit">
-        <b-form-group id="username" label="Username" label-for="i-1">
-          <b-form-input
-            id="i-1"
-            v-model="form.username"
-            required
-          ></b-form-input>
-        </b-form-group>
+            <b-button
+              id="submit_btn"
+              block
+              type="submit"
+              variant="success"
+              style="text-align: center"
+              >Login</b-button
+            >
+          </b-form>
 
-        <b-form-group id="password" label="Password" label-for="i-2">
-          <b-form-input
-            id="i-1"
-            v-model="form.username"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-button
-          id="submit_btn"
-          block
-          type="submit"
-          variant="success"
-          style="text-align: center"
-          >Login</b-button
-        >
-      </b-form>
-
-      <!-- debugging -->
-      <!-- <b-card class="mt-3" header="Form Data Result">
+          <!-- debugging -->
+          <!-- <b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">FORM DATA</pre>
     </b-card> -->
-    </b-jumbotron>
+        </b-jumbotron>
+      </div>
+    </div>
+    <div id="page_bottom"></div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'login',
   data() {
     return {
       form: {
-        username: 'temp',
-        password: 'temp'
+        username: '',
+        password: ''
       }
     }
+  },
+  mounted() {
+    // Happens when page is loaded
+    console.log('Login page has loaded')
   },
   methods: {
     // TODO Add working method to login user
     onSubmit(event) {
       event.preventDefault()
-      // alert(JSON.stringify(this.form))
-      alert(this.form.email)
     }
   }
 }
 </script>
 
 <style>
-#main-div {
-  padding-top: 100px;
-  padding-bottom: 25px;
-  padding-left: 25px;
-  padding-right: 25px;
-  background-color: white;
-  text-align: left;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  max-width: 750px;
-  margin: auto;
-}
-
-#login_form {
-  margin-top: 25px;
-  padding-top: 25px;
-  padding-bottom: 25px;
-  padding-left: 25px;
-  padding-right: 25px;
-  background-color: white;
-  border-radius: 5px;
-}
-
-#submit_btn {
-  font-size: x-large;
-  margin-top: 20px;
-  margin-bottom: 100px;
-}
+@import '../assets/styles/login-register_light.css';
 </style>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,65 +1,66 @@
 <template>
-  <div id="bg_register">
-    <div id="main-div">
-      <b-jumbotron id="register_background">
-        <template #header>
-          <b-form id="register_title"> Register</b-form>
-        </template>
-        <template #lead>
-          <b-form id="register_info">
-            <ul>
-              <li>
-                Select <strong>Pet owner</strong> if you are looking for a
-                caretaker for your pet
-              </li>
-              <li>
-                Select <strong>Pet lover</strong> if you would like to take care
-                of pets
-              </li>
-            </ul>
-            <hr class="my-4"
-          /></b-form>
-        </template>
+  <div>
+    <div class="header"></div>
+    <div id="bg_page">
+      <div id="main_div">
+        <b-jumbotron id="main_jumbo">
+          <template #header>
+            <b-form id="main_title"> Register</b-form>
+          </template>
 
-        <b-form id="register_form" @submit="onSubmit">
-          <b-form-group id="usertype" label="I want to be a," label-for="i-9">
-            <b-form-select
-              id="i-9"
-              v-model="form2.user"
-              :options="userType"
-              required
-              autofocus
-            ></b-form-select>
-          </b-form-group>
+          <b-form id="main_form" @submit="onSubmit">
+            <hr class="my-4" />
+            <b-form-group id="usertype" label="I want to be a," label-for="i-9">
+              <b-form-select
+                id="i-9"
+                v-model="form2.user"
+                :options="userType"
+                required
+                autofocus
+              ></b-form-select>
+            </b-form-group>
 
-          <b-form-group id="userName" label="Username" label-for="i-1">
-            <b-form-input
-              id="i-1"
-              v-model="form.userinfo.userName"
-              required
-            ></b-form-input>
-          </b-form-group>
+            <b-form>
+              <ul>
+                <li>
+                  Select <strong>Pet owner</strong> if you are looking for a
+                  caretaker for your pet
+                </li>
+                <li>
+                  Select <strong>Pet lover</strong> if you would like to take
+                  care of pets
+                </li>
+              </ul>
+            </b-form>
 
-          <b-form-group id="password" label="Password" label-for="i-2">
-            <b-form-input
-              id="i-2"
-              v-model="form.userinfo.password"
-              required
-              selected
-            ></b-form-input>
-          </b-form-group>
+            <b-form-group id="userName" label="Username" label-for="i-1">
+              <b-form-input
+                id="i-1"
+                v-model="form.userinfo.userName"
+                required
+              ></b-form-input>
+            </b-form-group>
 
-          <b-form-group id="fullname" label="Full name" label-for="i-3">
-            <b-form-input
-              id="i-3"
-              v-model="form.userinfo.fullname"
-              required
-            ></b-form-input>
-          </b-form-group>
+            <b-form-group id="password" label="Password" label-for="i-2">
+              <b-form-input
+                id="i-2"
+                v-model="form.userinfo.password"
+                required
+                selected
+              ></b-form-input>
+            </b-form-group>
 
-          <div class="accordion" role="tablist">
-            <!-- TODO Add options for adding a pet or a service when registering -->
-            <!--<b-card no-body class="mb-1">
+            <b-form-group id="fullname" label="Full name" label-for="i-3">
+              <b-form-input
+                id="i-3"
+                v-model="form.userinfo.fullname"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <div class="accordion" role="tablist">
+              <!-- TODO Add options for adding a pet or a service when registering -->
+              <!--<b-card no-body class="mb-1">
             <b-card-header header-tag="header" class="p-1" role="tab">
               <b-button block v-b-toggle.accordion-2 variant="info"
                 >Pet owner/lover Info</b-button
@@ -77,104 +78,106 @@
             </b-collapse>
           </b-card> -->
 
-            <b-card no-body class="mb-1">
-              <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle.accordion-1 variant="primary"
-                  >Contact Info (optional)
-                </b-button></b-card-header
-              >
-              <b-collapse
-                id="accordion-1"
-                accordion="my-accordion"
-                role="tabpanel"
-              >
-                <b-card-body>
-                  <b-form-group id="email" label="Email" label-for="i-4">
-                    <b-form-input
-                      id="i-4"
-                      v-model="form.userinfo.contactInfo.email"
-                    ></b-form-input>
-                  </b-form-group>
+              <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                  <b-button block v-b-toggle.accordion-1 variant="primary"
+                    >Contact Info (optional)
+                  </b-button></b-card-header
+                >
+                <b-collapse
+                  id="accordion-1"
+                  accordion="my-accordion"
+                  role="tabpanel"
+                >
+                  <b-card-body>
+                    <b-form-group id="email" label="Email" label-for="i-4">
+                      <b-form-input
+                        id="i-4"
+                        v-model="form.userinfo.contactInfo.email"
+                      ></b-form-input>
+                    </b-form-group>
 
-                  <b-form-group
-                    id="phoneNumber"
-                    label="Phone number"
-                    label-for="i-5"
-                  >
-                    <b-form-input
-                      id="i-5"
-                      v-model="form.userinfo.contactInfo.phoneNumber"
-                    ></b-form-input>
-                  </b-form-group>
+                    <b-form-group
+                      id="phoneNumber"
+                      label="Phone number"
+                      label-for="i-5"
+                    >
+                      <b-form-input
+                        id="i-5"
+                        v-model="form.userinfo.contactInfo.phoneNumber"
+                      ></b-form-input>
+                    </b-form-group>
 
-                  <b-form-group id="city" label="City" label-for="i-6">
-                    <b-form-input
-                      id="i-6"
-                      v-model="form.userinfo.contactInfo.address.city"
-                    ></b-form-input>
-                  </b-form-group>
+                    <b-form-group id="city" label="City" label-for="i-6">
+                      <b-form-input
+                        id="i-6"
+                        v-model="form.userinfo.contactInfo.address.city"
+                      ></b-form-input>
+                    </b-form-group>
 
-                  <b-form-group
-                    id="streetName"
-                    label="Street name"
-                    label-for="i-7"
-                  >
-                    <b-form-input
-                      id="i-7"
-                      v-model="form.userinfo.contactInfo.address.streetName"
-                      placeholder=""
-                    ></b-form-input>
-                  </b-form-group>
+                    <b-form-group
+                      id="streetName"
+                      label="Street name"
+                      label-for="i-7"
+                    >
+                      <b-form-input
+                        id="i-7"
+                        v-model="form.userinfo.contactInfo.address.streetName"
+                        placeholder=""
+                      ></b-form-input>
+                    </b-form-group>
 
-                  <b-form-group
-                    id="streetNumber"
-                    label="Street Number"
-                    label-for="i-8"
-                  >
-                    <b-form-input
-                      id="i-8"
-                      v-model="form.userinfo.contactInfo.address.streetNumber"
-                    ></b-form-input>
-                  </b-form-group>
-                </b-card-body>
-              </b-collapse>
-            </b-card>
-          </div>
+                    <b-form-group
+                      id="streetNumber"
+                      label="Street Number"
+                      label-for="i-8"
+                    >
+                      <b-form-input
+                        id="i-8"
+                        v-model="form.userinfo.contactInfo.address.streetNumber"
+                      ></b-form-input>
+                    </b-form-group>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
+            </div>
 
-          <b-button
-            id="submit_btn"
-            block
-            type="submit"
-            variant="success"
-            style="text-align: center"
-            >Register Account</b-button
-          >
-          <hr class="my-4" />
-          <p style="text-align: center">
-            Do you already have an account?
-            <router-link to="/login">Login here</router-link>
-          </p>
+            <b-button
+              id="submit_btn"
+              block
+              type="submit"
+              variant="success"
+              style="text-align: center"
+              >Register Account</b-button
+            >
+            <hr class="my-4" />
+            <p style="text-align: center">
+              Do you already have an account?
+              <router-link to="/login">Login here</router-link>
+            </p>
 
-          <div id="test_image">
-            <img
-              src="../assets/images/sammy-delivery.png"
-              alt="..."
-              style="
-                display: block;
-                margin-left: auto;
-                margin-right: auto;
-                width: 100%;
-              "
-            />
-          </div>
-        </b-form>
-      </b-jumbotron>
+            <div id="test_image">
+              <img
+                src="../assets/images/sammy-delivery.png"
+                alt="..."
+                style="
+                  display: block;
+                  margin-left: auto;
+                  margin-right: auto;
+                  width: 100%;
+                "
+              />
+            </div>
+          </b-form>
+        </b-jumbotron>
 
-      <!-- debugging -->
-      <!-- <b-card class="mt-3" header="Form Data Result">
+        <!-- debugging -->
+        <!-- <b-card class="mt-3" header="Form Data Result">
       <pre class="m-0">FORM DATA</pre>
     </b-card> -->
+      </div>
     </div>
+    <div class="footer"></div>
   </div>
 </template>
 
@@ -217,11 +220,7 @@ export default {
     onSubmit(event) {
       // TODO Add working method to POST PetLover and PetOwner
       event.preventDefault()
-      // alert(JSON.stringify(this.form))
-      // alert(this.form.email)
-      // if (userType === 'Pet owner') {
-      // }
-      console.log(this.form2.user)
+      console.log(this.form2.user) // debugging
       if (this.form2.user === 'Pet owner') {
         Api.post('/v1/petowners', this.form)
           .then((response) => this.form)
@@ -249,7 +248,7 @@ export default {
 </script>
 
 <style>
-@import '../assets/styles/register_light.css';
+@import '../assets/styles/login-register_light.css';
 
 #test_image {
   margin-top: -25px;
