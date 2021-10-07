afef350fe0084dabdb773b6388a2b4393a277188
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-10-07 22:34:29 +0200

    change the design of the pet page

--- client/src/views/Pets.vue
+++ client/src/views/Pets.vue
@@ -1,41 +1,23 @@
 <template>
   <b-container>
-    <b-row class="pet_cards" >
-      <b-col class="card_column" v-for="pet in pets" :key="pet._id" cols="12" sm="6" md="4">
-        <b-card img-src="https://ichef.bbci.co.uk/news/976/cpsprodpb/1086B/production/_115619676_dog2.jpg"
-        img-alt="Image"
-        img-top>
-          <template #header>
-            <h5>{{pet.name}}</h5>
-          </template>
-          <b-card-body class="card-body">
-            <b-card-text v-if="pet.type">Type: {{pet.type}}</b-card-text>
-            <b-card-text v-if="gender">Gender: {{pet.gender}}</b-card-text>
-            <b-card-text v-if="pet.allergies">
-              Allergies:
-              <ul v-for="(allergy, index) in pet.allergies" :key="index">
-                <li>{{allergy}}</li>
-              </ul>
-            </b-card-text>
-            <b-card-text v-if="pet.foodPreferences">
-              Food Preference:
-              <ul v-for="(food, index) in pet.foodPreferences" :key="index">
-                <li>{{food}}</li>
-              </ul>
-            </b-card-text>
-            <b-card-text v-if="pet.petItems">
-              Items:
-              <ul v-for="(item, index) in pet.petItems" :key="index">
-                <li>{{item}}</li>
-              </ul>
-            </b-card-text>
-          </b-card-body>
+    <b-row v-for="pet in pets" :key="pet._id">
+      <b-col md="12">
+        <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left>
+        <b-card-text >
+          <p>Name:
+          <p class="attributes">{{pet.name}}
+          <p>Type:
+          <p class="attributes">{{pet.type}}
+          <p>Gender:
+          <p class="attributes">{{pet.gender}}
+          <p v-if="pet.allergies">Allergies:
+          </p>
+        </b-card-text>
         </b-card>
       </b-col>
     </b-row>
   </b-container>
-</template>>
-
+</template>
 <script>
 import { Api } from '@/Api'
 
