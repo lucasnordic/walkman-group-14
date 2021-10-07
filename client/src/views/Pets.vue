972c4a5b511a2fc957616b26122df9eae227b5c1
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-10-07 09:39:30 +0200

    refactore the pet view part of the code

--- client/src/views/Pets.vue
+++ client/src/views/Pets.vue
@@ -1,38 +1,39 @@
 <template>
-  <container>
-    <container class="pet_view" v-for="pet in pets" :key="pet._id">
-      <b-card-group>
-        <b-card title="pet.name">
-          <b-card-text>
-            <b-list-group>
-              <b-list-group-item>Type: {{pet.type}}</b-list-group-item>
-              <b-list-group-item>Gender: {{pet.gender}}</b-list-group-item>
-              <b-list-group-item>
-                Allergies:
-                <ul v-for="(allergy, index) in pet.allergies" :key="index">
-                  <li>{{allergy}}</li>
-                </ul>
-              </b-list-group-item>
-              <b-list-group-item>
-                Food Preferences:
-                <ul v-for="(food, index) in pet.foodPreferences" :key="index">
-                  <li>{{food}}</li>
-                </ul>
-              </b-list-group-item>
-              <b-list-group-item>
-                Items:
-                <ul v-for="(item, index) in pet.petItems" :key="index">
-                  <li>{{item}}</li>
-                </ul>
-              </b-list-group-item>
-            </b-list-group>
-          </b-card-text>
+  <b-container>
+    <b-row class="pet_cards" >
+      <b-col class="card_column" v-for="pet in pets" :key="pet._id" cols="12" sm="6" md="4">
+        <b-card img-src="https://ichef.bbci.co.uk/news/976/cpsprodpb/1086B/production/_115619676_dog2.jpg"
+        img-alt="Image"
+        img-top>
+          <template #header>
+            <h5>{{pet.name}}</h5>
+          </template>
+          <b-card-body class="card-body">
+            <b-card-text v-if="pet.type">Type: {{pet.type}}</b-card-text>
+            <b-card-text v-if="gender">Gender: {{pet.gender}}</b-card-text>
+            <b-card-text v-if="pet.allergies">
+              Allergies:
+              <ul v-for="(allergy, index) in pet.allergies" :key="index">
+                <li>{{allergy}}</li>
+              </ul>
+            </b-card-text>
+            <b-card-text v-if="pet.foodPreferences">
+              Food Preference:
+              <ul v-for="(food, index) in pet.foodPreferences" :key="index">
+                <li>{{food}}</li>
+              </ul>
+            </b-card-text>
+            <b-card-text v-if="pet.petItems">
+              Items:
+              <ul v-for="(item, index) in pet.petItems" :key="index">
+                <li>{{item}}</li>
+              </ul>
+            </b-card-text>
+          </b-card-body>
         </b-card>
-      </b-card-group>
-    </container>
-    <container class="registration">
-    </container>
-  </container>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>>
 
 <script>
@@ -49,15 +50,7 @@ export default {
   },
   data() {
     return {
-      pets: {},
-      pet: {
-        name: null,
-        type: null,
-        gender: null,
-        allergies: null,
-        foodPreferences: null,
-        petItems: null
-      }
+      pets: {}
     }
   },
   methods: {
@@ -68,9 +61,6 @@ export default {
       Api.post('/petowners/' + this.$route.params.id + '/pets', this.pet)
         .then(res => this.pets)
     },
-    /* editPet(petId) {
-      Api.put('/petowners/pets/' + petId, this.editedPet)
-    }, */
     openForm() {
       document.getElementById('popupForm').style.display = 'block'
     },
