5b29e8ffd341d1417140fdf847c46bc85212e429
Maryam Esmaeili Darestani <maryame@student.chalmers.se>
2021-10-12 09:44:28 +0200

    refactor petOwner profile

--- client/src/views/PetOwnerProfile.vue
+++ client/src/views/PetOwnerProfile.vue
@@ -1,8 +1,137 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col></b-col>
+  <b-container v-if="petOwner" fluid>
+    <b-row v-model="petOwner">
+      <b-col md="12">
+        <b-card class="greeting-user">
+        <b-card-text>
+          Hello {{petOwner.userinfo.username}}!
+        </b-card-text>
+        </b-card>
+        <b-card class="user-info">
+          <b-card-text>
+            <p id="header">Profile information:
+            <p class="key"> Username:
+              <span class="value"> {{petOwner.userinfo.username}}</span>
+            </p>
+            <p class="key"> Password:
+              <span class="value"> {{petOwner.userinfo.password}}</span>
+            </p>
+            <p class="key"> Full Name:
+              <span class="value"> {{petOwner.userinfo.fullname}}</span>
+            </p>
+            <p class="key"> Email:
+              <span class="value"> {{petOwner.userinfo.contactInfo.email}}</span>
+            </p>
+            <p class="key"> Phone Number:
+              <span class="value"> {{petOwner.userinfo.contactInfo.phoneNumber}}</span>
+            </p>
+            <p class="key"> City:
+              <span class="value"> {{petOwner.userinfo.contactInfo.address.city}}</span>
+            </p>
+            <p class="key"> Street Name:
+              <span class="value"> {{petOwner.userinfo.contactInfo.address.streetName}}</span>
+            </p>
+            <p class="key"> Street Number:
+              <span class="value"> {{petOwner.userinfo.contactInfo.address.streetNumber}}</span>
+            </p>
+            <p class="key"> Description:
+              <p class="value"> {{petOwner.aboutMe}}
+            </p>
+            <b-button variant="danger" id="delete-button" @click="delAcc()">Delete Account</b-button>
+            <b-button variant="primary" id="edit-button" v-b-modal.edit-user>Edit Information</b-button>
+          </b-card-text>
+        </b-card>
+      </b-col>
     </b-row>
+    <b-row id="service-row">
+      <b-col md="12">
+        <b-button  variant="success" @click="petPage()" id="service-btn">Your Pets</b-button>
+      </b-col>
+    </b-row>
+    <b-modal id="edit-user" title="Edit Information" ref="editModal">
+      <b-container>
+        <b-row>
+          <b-col md="3">
+            <label>Username:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.username"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>Password:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.password"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>fullname:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.fullname"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>Email:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.contactInfo.email"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>Phone Number:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.contactInfo.phoneNumber"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>City:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.contactInfo.address.city"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>Street Name:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.contactInfo.address.streetName"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>Street Number:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.userinfo.contactInfo.address.streetNumber"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="3">
+            <label>Description:</label>
+          </b-col>
+          <b-col md="9">
+            <b-form-input v-model="petOwner.aboutMe"></b-form-input>
+          </b-col>
+        </b-row>
+      </b-container>
+      <template #modal-footer>
+      <b-button size="lg" variant="success" @click="edit()">
+        Save
+      </b-button>
+      <b-button size="lg" variant="outline-secondary" @click="close()">
+        Cancel
+      </b-button>
+    </template>
+    </b-modal>
   </b-container>
 </template>
 
@@ -12,8 +141,6 @@ import { Api } from '@/Api'
 export default {
   name: 'petOwner',
   mounted() {
-    // Happens when page is loaded
-    console.log('Yoyo!')
     Api.get('/petowners/' + this.$route.params.id).then((res) => {
       console.log(res)
       this.petOwner = res.data
@@ -21,22 +148,41 @@ export default {
   },
   data() {
     return {
-      petOwner: null
+      petOwner: {
+        userinfo: {
+          username: null,
+          password: null,
+          fullname: null,
+          contactInfo: {
+            email: null,
+            phoneNumber: null,
+            address: {
+              city: null,
+              streetName: null,
+              streetNumber: null
+            }
+          }
+        },
+        aboutMe: null,
+        _pets: null
+      }
     }
   },
   methods: {
-    editSNum() {
-      this.petOwner.userinfo.contactInfo.address.streetNum = this.sNumUpdate
+    edit() {
       Api.put('/petowners/' + this.$route.params.id, this.petOwner).then(
         (res) => this.petLover
       )
-      this.sNumUpdate = null
+      this.close()
     },
-    deleteAcc() {
+    delAcc() {
       Api.delete('/petowners/' + this.$route.params.id)
     },
     petPage() {
       this.$router.push('/profile/petowners/' + this.$route.params.id + '/pets')
+    },
+    close() {
+      this.$refs.editModal.hide()
     }
   }
 }
